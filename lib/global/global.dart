8e4f056ff2b9b1e047fdeed4d86e0f52de316466
saranmac <matoda4113>
2023-09-02 17:50:39 +0900

    gl

--- lib/global/global.dart
+++ lib/global/global.dart
@@ -15,4 +15,5 @@ Logger logger = Logger(
   ),
 );
 
+//깃허브로 받으신분은 걍 제 에세스 코드 쓰셔도 뭐라안하겠음..ㅋ 뭐 돈나가는것도 아니고
 String accessCode = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ODMwMjRkYzgwYzQ2NGMxYWYwN2ExYjk4ZWJhY2UxNyIsInN1YiI6IjY0ZTg1ZjA3YzYxM2NlMDEyY2M1MDY5MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sEg3EDwlugvxSKZewLTZ_SKfTIyZWrSHXpBODuUPEmA";
